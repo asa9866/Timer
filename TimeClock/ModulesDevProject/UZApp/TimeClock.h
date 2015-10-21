3cce0ba8bba845b8056bb7efca5f5be407993ed8
likun <lkungis@163.com>
2015-10-21 10:17:13 +0800

    添加

---
+++ TimeClock/ModulesDevProject/UZApp/TimeClock.h
@@ -0,0 +1,14 @@
+//
+//  TimeClock.h
+//  TimeClock
+//
+//  Created by sipo on 15/10/20.
+//  Copyright (c) 2015年 bnu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "UZEngine/UZModule.h"
+
+@interface TimeClock : UZModule
+
+@end
